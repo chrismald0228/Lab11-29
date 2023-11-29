510c05c2e712f8bc7f4504a0cd40d1e8f5a01441
chrismald0228 <149415670+chrismald0228@users.noreply.github.com>
2023-11-29 13:02:47 -0600

    finished example 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,70 @@
+import java.util.*;
+//example 1
+//example 2
+//example 3
+interface Encryptable{
+    void encrypt();
+    void decrypt();
+}
+
+class Secret implements Encryptable{
+    private String message;
+    private int shift;
+    private boolean encrypted;
+
+    public Secret(){
+        message = "";
+        shift = 0;
+        encrypted = false;
+    }
+    public Secret(String mes, int newShift){
+        message = mes;
+        shift = newShift;
+    }
+    public void encrypt(){
+        String encryptString = "";
+        for(int i=0;i<message.length();i++){
+            int ascii = message.charAt(i);
+            int nAscii = ascii + shift;
+            char nChar = (char) nAscii;
+
+            encryptString+=nChar;
+        }
+        message = encryptString;
+    }
+    public void decrypt(){
+        String encryptString = "";
+        for(int i=0;i<message.length();i++){
+            int ascii = message.charAt(i);
+            int nAscii = ascii - shift;
+            char nChar = (char) nAscii;
+
+            encryptString += nChar;
+        }
+        message = encryptString;
+    }
+
+    public void setMessage(String newMessage){
+        message = newMessage;
+    }
+    public void setShift(int newShift){
+        shift = newShift;
+    }
+    public String getMessage(){
+        return message;
+    }
+
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random rand = new Random();
+        Secret secret = new Secret("gang", rand.nextInt(25));
+        System.out.println(secret.getMessage());
+        secret.encrypt();
+        System.out.println(secret.getMessage());
+        secret.decrypt();
+        System.out.println(secret.getMessage());
+
     }
 }
